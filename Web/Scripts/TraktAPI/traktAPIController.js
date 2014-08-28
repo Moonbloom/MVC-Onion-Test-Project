40050fa1161e5fa548080a396231b759ba698fcd
Moonboom <kasperliholm@hotmail.com>
2014-08-28 15:23:59 +0200

    Figured out why TraktTV series/movies search api doesn't work Removed unneeded code

--- Web/Scripts/TraktAPI/traktAPIController.js
+++ Web/Scripts/TraktAPI/traktAPIController.js
@@ -3,16 +3,17 @@
 traktAPIController.controller('traktAPIController', ['$scope', "$http", function ($scope, $http) {
 
     $scope.getData = function () {
-        //var today = new Date();
+        var today = new Date();
         //Create the date string and ensure leading zeros if required
-        //var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+        var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
         //$http.jsonp('http://api.trakt.tv/calendar/premieres.json' + '/02110f747fe9f8d587f31452403d5e69' + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
-        $http.jsonp('http://api.trakt.tv/search/shows.json' + '/02110f747fe9f8d587f31452403d5e69' + '?query=' + $scope.searchtext + '/?callback=JSON_CALLBACK').success(function (data) {
+
+        $http.get('http://api.trakt.tv/search/shows.json' + '/02110f747fe9f8d587f31452403d5e69' + '?query=' + $scope.searchtext).success(function (data) {
             console.log(data);
 
             $scope.search = data;
-        }).error(function (error) {
-            alert(error);
+        }).error(function (data, status) {
+            alert(data, status);
         });
     }
     }
